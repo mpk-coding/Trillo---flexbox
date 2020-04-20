6315051417c6a974204825ee37a438bd32079539
nadmiar-wszechswiata <thamak@gmail.com>
2020-04-20 14:53:41 +0200

    recommended search results, truncated search recommendation tooltip, gallery figcaption on hover
    recommended search results:
    search__suggested @components/search.scss
    (line 115)
    -------------------
    truncated text tooltip in search__suggested
    @js/main.js
    (line 6)
    -------------------
    
    hover on gallery photo
    gallery__caption
    @components/gallery.scss
    (line 27)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
+//to prevent animation transitions firing off on window load
 window.onload = function preload() {
   var layout = document.getElementsByClassName('preload')[0];
   layout.classList.remove('preload');
+
+//search__suggested truncated text tooltips
+  var items = document.getElementsByClassName('search__list-item');
+  for (var i = 0; i < items.length; i++) {
+    if (0 > items[i].clientWidth - items[i].scrollWidth) {
+    items[i].addEventListener('mouseover', function (i) {
+    tooltipText = this.innerText;
+    this.setAttribute("title", tooltipText)
+    });
+    }
+  }
 }
